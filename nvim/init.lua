60fa27b0279701af6b3aee516f4e6a4d33deb494
Robin <robin@macbookair.home>
2022-11-28 01:49:59 +0100

    Updated nvim config; Bugfixes for auto-intent and intent spaces

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,7 +12,15 @@ local is_win = has "win32"
 
 if is_mac then
         require('macos')
+vim.o.autoindent = true
+vim.o.expantab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 end
 if is_win then
         require('windows')
+vim.o.autoindent = true
+vim.o.expantab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 end
